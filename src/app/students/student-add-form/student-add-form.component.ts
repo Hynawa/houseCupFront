08c6ef013ab71a40e16cefd98ef5d4783e310607
Pietro Marinari <pietro.marinari@gmail.com>
2024-12-11 09:14:24 +0100

    formattato codice

--- src/app/students/student-add-form/student-add-form.component.ts
+++ src/app/students/student-add-form/student-add-form.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HouseDto } from '../../model/houses/house-dto';
 import { CourseDto } from '../../model/courses/course-dto';
-import { StudentService } from '../../model/students/student-service';
 import { HouseService } from '../../model/houses/house-service';
 import { CourseService } from '../../model/courses/course-service';
 
@@ -12,16 +11,19 @@ import { CourseService } from '../../model/courses/course-service';
   templateUrl: './student-add-form.component.html',
   styleUrl: './student-add-form.component.css'
 })
+
 export class StudentAddFormComponent implements OnInit{
   studentForm!: FormGroup;
   houses: HouseDto[] = [];
   courses: CourseDto[] = [];
+  
   constructor(private houseService: HouseService, private courseService: CourseService, private fb: FormBuilder){}
+
   ngOnInit(): void {
     this.studentForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       lastName: ['', [Validators.required, Validators.minLength(2)]],
-      
+
     });
     this.houseService.getHouses().subscribe({
       next: hs => this.houses = hs,
